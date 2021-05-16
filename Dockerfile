a7665d53f00d5da9ce0058e0f13ae113092e7b44
Nguyen Dinh Bien <44922242+biennd279@users.noreply.github.com>
2021-05-16 22:29:56 +0700

    Add momo payment method (#3)
    * init 3 method payment
    
    * add payment method
    
    * ignore xdebug.log
    
    * reface namespace and url

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
 FROM biennd279/php-fpm-magento2
 
-COPY . /var/www/html
+# Install Xdebug
+RUN apk add --no-cache $PHPIZE_DEPS \
+    && pecl install xdebug-3.0.0 \
+    && docker-php-ext-enable xdebug
+
+# Configure Xdebug
+RUN echo "xdebug.start_with_request=yes" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.mode=debug" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.log=/var/www/html/xdebug/xdebug.log" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.discover_client_host=1" >> /usr/local/etc/php/conf.d/xdebug.ini \
+    && echo "xdebug.client_port=9000" >> /usr/local/etc/php/conf.d/xdebug.ini
---
+++ app/code/Dabilo/Payment/Gateway/Momo/Http/AbstractTransferFactory.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace Dabilo\Payment\Gateway\Momo\Http;
+
+
+use Dabilo\Payment\Gateway\Momo\Helper\Authorization;
+use Laminas\Serializer\Adapter\Json;
+use Magento\Payment\Gateway\ConfigInterface;
+use Magento\Payment\Gateway\Http\TransferBuilder;
+use Magento\Payment\Gateway\Http\TransferFactoryInterface;
+
+abstract class AbstractTransferFactory implements TransferFactoryInterface
+{
+    /**
+     * @var ConfigInterface
+     */
+    protected ConfigInterface $config;
+
+    /**
+     * @var TransferBuilder
+     */
+    protected TransferBuilder $transferBuilder;
+
+    /**
+     * Authenticate & generate Headers
+     *
+     * @var Authorization
+     */
+    private Authorization $authorization;
+
+    /**
+     * @var Json
+     */
+    protected Json $serializer;
+
+    /**
+     * @var string
+     */
+    protected ?string $urlPath;
+
+    /**
+     * AbstractTransferFactory constructor.
+     *
+     * @param ConfigInterface $config
+     * @param TransferBuilder $transferBuilder
+     * @param Json            $serializer
+     * @param Authorization   $authorization
+     * @param null            $urlPath
+     */
+    public function __construct(
+        ConfigInterface $config,
+        TransferBuilder $transferBuilder,
+        Json $serializer,
+        Authorization $authorization,
+        $urlPath = null
+    ) {
+        $this->config          = $config;
+        $this->transferBuilder = $transferBuilder;
+        $this->authorization   = $authorization;
+        $this->serializer      = $serializer;
+        $this->urlPath         = $urlPath;
+    }
+
+    /**
+     * @return boolean
+     */
+    protected function isSandboxMode(): bool
+    {
+        return (bool)$this->config->getValue('sandbox_flag');
+    }
+
+    /**
+     * @return Authorization
+     */
+    protected function getAuthorization(): Authorization
+    {
+        return $this->authorization;
+    }
+}
